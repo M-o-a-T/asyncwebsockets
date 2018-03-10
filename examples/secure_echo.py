9b20b9bcc51279087f9c118ed2b86c070b5499ff
Matthias Urlichs <matthias@urlichs.de>
2018-03-10 18:17:11 +0100

    add simple echo examples

---
+++ examples/secure_echo.py
@@ -0,0 +1,18 @@
+import trio
+
+from trio_websockets.client import connect_websocket
+from trio_websockets.ws import WebsocketConnectionEstablished, WebsocketBytesMessage
+
+async def test():
+    sock = await connect_websocket("wss://echo.websocket.org", reconnecting=False)
+    async for message in sock:
+        print("Event received", message)
+        if isinstance(message, WebsocketConnectionEstablished):
+            await sock.send_message(b"test")
+
+        elif isinstance(message, WebsocketBytesMessage):
+            print("Got response:", message.data)
+            await sock.aclose(code=1000, reason="Thank you!")
+
+trio.run(test)
+
